ade50d3de95c0a2b48dac6fc88b802a374e52bef
CESCO <cescovalledev@gmail.com>
2024-06-20 22:22:33 -0600

    beginning step 3

--- app/ui/SelectAddons.tsx
+++ app/ui/SelectAddons.tsx
@@ -1,8 +1,122 @@
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import clsx from "clsx";
+import { useFormContext } from "react-hook-form";
+import { useWizard } from "react-use-wizard";
+
+const items = [
+  {
+    id: "online-service",
+    label: "Online service",
+    description: "Access to multiplayer games",
+    monthlyPrice: 1,
+    yearlyPrice: 10,
+  },
+  {
+    id: "larger-storage",
+    label: "Larger Storage",
+    description: "Extra 1TB of cloud save",
+    monthlyPrice: 2,
+    yearlyPrice: 20,
+  },
+  {
+    id: "customizable-profile",
+    label: "Customizable Profile",
+    description: "Custom theme on your profile",
+    monthlyPrice: 2,
+    yearlyPrice: 20,
+  },
+];
+
 const SelectAddons = () => {
+  const form = useFormContext();
+  const { nextStep, previousStep } = useWizard();
+
+  const onSubmit = () => {
+    console.log(form.getValues());
+    nextStep();
+  };
+
   return (
     <>
       <h1 className="font-bold text-xl">Personal Information</h1>
       <p>Please provide your name, email address, and phone number.</p>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 mt-4">
+          <FormField
+            control={form.control}
+            name="addons"
+            render={() => (
+              <FormItem>
+                {items.map((item) => (
+                  <FormField
+                    key={item.id}
+                    control={form.control}
+                    name="addons"
+                    render={({ field }) => (
+                      <FormItem
+                        key={item.id}
+                        className={clsx(
+                          "flex flex-row items-start space-x-3 border p-4 rounded space-y-0",
+                          {
+                            "border-accent":
+                              field.value?.includes(item.id) === true,
+                          }
+                        )}
+                      >
+                        <FormControl>
+                          <Checkbox
+                            checked={field.value?.includes(item.id)}
+                            onCheckedChange={(checked) => {
+                              return checked
+                                ? field.onChange([...field.value, item.id])
+                                : field.onChange(
+                                    field.value?.filter(
+                                      (value: string) => value !== item.id
+                                    )
+                                  );
+                            }}
+                          />
+                        </FormControl>
+                        <FormLabel className="font-normal flex">
+                          <div className="flex flex-col gap-2">
+                            <p className="font-bold text-accent">
+                              {item.label}
+                            </p>
+                            <p>{item.description}</p>
+                          </div>
+                          <p>{`+$${item.monthlyPrice}`}</p>
+                        </FormLabel>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                ))}
+              </FormItem>
+            )}
+          />
+          <div className="fixed bottom-0 right-0 left-0 bg-primary w-full justify-between p-4 flex">
+            <Button
+              onClick={previousStep}
+              variant={"link"}
+              className="text-muted"
+            >
+              Go Back
+            </Button>
+            <Button type="submit" variant={"secondary"}>
+              Next Step
+            </Button>
+          </div>
+        </form>
+      </Form>
     </>
   );
 };
